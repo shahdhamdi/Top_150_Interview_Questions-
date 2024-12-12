2fea4cee6bb86035c4ba5522ed0b87e5376be854
Shahd Hamdi <shahd.hamdi162@gmail.com>
2024-12-11 20:44:18 -0800

    Time: 0 ms (100.00%) | Memory: 14.3 MB (23.40%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int count = 1; // Initialize count to track the valid elements
-        for (int i = 1; i < nums.size(); i++) {
-            // Check if the current element can be added
-            if (count == 1 || nums[i] != nums[count - 2]) {
-                nums[count] = nums[i]; // Add the element to the result
-                count++;
-            }
+        int count=1;
+     for(int i=1;i<nums.size();i++)
+     {
+        if(count==1||nums[i]!=nums[count-2])
+        {
+            nums[count]=nums[i];
+            count++;
         }
-        return count; // Return the number of valid elements
+     }
+     return count;    
     }
 };
