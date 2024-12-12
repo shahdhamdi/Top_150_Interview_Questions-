a284cc64e161433af2b19d73baa93e5ac6059a36
Shahd Hamdi <shahd.hamdi162@gmail.com>
2024-12-11 20:44:57 -0800

    Time: 4 ms (27.83%) | Memory: 28.3 MB (11.41%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
         int majority_element;
         unordered_map<int, int> freq;
-        
-        // Count frequency of each element
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++)
+        {
             freq[nums[i]]++;
         }
-        
-        // Find the element with frequency greater than n/2
-        for (int i = 0; i < n; i++) {
-            if (freq[nums[i]] > n / 2) {
-                majority_element = nums[i];
+        for(int i=0;i<n;i++)
+        {
+            if(freq[nums[i]]>n/2)
+            {
+                majority_element=nums[i];
                 break;
             }
         }
-        
         return majority_element;
     }
 };
