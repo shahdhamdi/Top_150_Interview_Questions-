b73b251cbcc2935a91177ecc40a6ff87fa51cbaf
Shahd Hamdi <shahd.hamdi162@gmail.com>
2024-12-11 20:47:45 -0800

    Time: 0 ms (100.00%) | Memory: 12.2 MB (19.50%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        sort(citations.begin(), citations.end(), greater<>());
-        for (int i = 0; i < n; i++) {
-            if (i >= citations[i]) {
-                return i;
-            }
+       int n=citations.size();
+       sort(citations.begin(),citations.end(),greater<>());
+       for(int i=0;i<n;i++)
+       {
+        if(i>=citations[i])
+        {
+          return i;
         }
-        return n;
+       }
+       return n;
     }
 };
