f4917051db3f27ecf90407c0253ba52ac2c29fb2
Shahd Hamdi <shahd.hamdi162@gmail.com>
2024-12-11 20:46:19 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (31.57%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,13 +3,12 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit=0;
         int n=prices.size();
-        // Traverse through the prices list
         for(int i=1;i<n;i++)
         {
             int temp=0;
-            temp=prices[i]-prices[i-1]; // Calculate the profit from buying and selling on adjacent days
+            temp=prices[i]-prices[i-1];
             if(temp>0){
-                profit+=temp; // Add the positive profit to the total profit
+                profit+=temp;
             }
         }
         return profit;
