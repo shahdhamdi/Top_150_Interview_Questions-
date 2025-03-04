374ed247507f87270d654f7a6a6b4dd1cd3c62f6
Shahd Hamdi <shahd.hamdi162@gmail.com>
2025-03-04 03:38:30 +0200

    Time: 4 ms (27.33%) | Memory: 17.9 MB (74.65%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,8 +1,14 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         lst=[]
+        hashmap={}
+        for num in nums2:
+            if num not in hashmap:
+                hashmap[num]=1
+            else:
+                hashmap[num]+=1
         for num in nums1:
-            if num in nums2:
+            if num in hashmap and hashmap[num]>0:
                 lst.append(num)
-                nums2.remove(num)
+                hashmap[num]-=1
         return lst
